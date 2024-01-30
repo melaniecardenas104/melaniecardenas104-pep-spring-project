9fe01584804b9dda5f570f0257133ca677db62d6
Melanie Cardenas <143232669+melaniecardenas104@users.noreply.github.com>
2024-01-29 18:42:02 -0600

    Update AccountService.java

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,24 @@
-package com.example.service;
+package Service;
+
+import DAO.AccountDAO;
+import Model.Account;
 
 public class AccountService {
+    public AccountDAO accountDAO;
+
+    public AccountService() {
+        accountDAO = new AccountDAO();
+    }
+    public AccountService(AccountDAO accountDAO) {
+        this.accountDAO = accountDAO;
+    }
+
+    public Account insertUser(Account account) {
+        return accountDAO.insertUser(account);
+    }
+
+    public Account checkLogin(Account account) {
+        return accountDAO.checkLogin(account);
+    }
+    
 }
